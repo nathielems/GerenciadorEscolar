1ed2905d5560a90948263a25ff29c1a38b5c5971
Henrique Gudaites <52537422+Gudaites@users.noreply.github.com>
2020-02-10 08:57:43 -0300

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,21 +3,35 @@
 @section('content')
 
 <div class="conteudo">
-    <div class="card-columns d-flex justify-content-center">
-        <div class="card text-center" style="width:300px">
-            <img class="card-img-top" src="{{ URL::to('/imagens/cadastro.jfif') }}" alt="">
-            <div class="card-body">
-                <h4 class="card-title">Cadastro Exemplo</h4>
-                <p class="card-text"></p>
-                <a href="{{ route('cadastro') }}" class="btn btn-primary stretched-link">Link cadastro</a>
+    <div class="row d-flex justify-content-around">
+        <div class="col-sm-3">
+            <div class="card text-center">
+                <img class="card-img-top" src="{{ URL::to('/imagens/images.png') }}" alt="">
+                <div class="card-body">
+                    <h4 class="card-title">Cadastro Exemplo</h4>
+                    <p class="card-text"></p>
+                    <a href="{{ route('cadastro') }}" class="btn btn-primary stretched-link">Link cadastro</a>
+                </div>
             </div>
         </div>
-        <div class="card text-center" style="width:300px">
-            <img class="card-img-top" src="{{ URL::to('/imagens/images.png') }}" alt="">
-            <div class="card-body">
-                <h4 class="card-title">Lista Exemplo</h4>
-                <p class="card-text"></p>
-                <a href="{{ route('lista') }}" class="btn btn-primary stretched-link">Link lista</a>
+        <div class="col-sm-3">
+            <div class="card text-center">
+                <img class="card-img-top" src="{{ URL::to('/imagens/images.png') }}" alt="">
+                <div class="card-body">
+                    <h4 class="card-title">Lista Exemplo</h4>
+                    <p class="card-text"></p>
+                    <a href="{{ route('lista') }}" class="btn btn-primary stretched-link">Link lista</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-3">
+            <div class="card text-center">
+                <img class="card-img-top" src="{{ URL::to('/imagens/images.png') }}" alt="">
+                <div class="card-body">
+                    <h4 class="card-title">PDF Exemplo</h4>
+                    <p class="card-text"></p>
+                    <a href="{{ route('pdf') }}" class="btn btn-primary stretched-link">Link lista</a>
+                </div>
             </div>
         </div>
     </div>
